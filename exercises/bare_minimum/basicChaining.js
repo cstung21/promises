2b271bf0966f1a591dd3d3603ee9174d9590dc3c
cstung21 <cstung21@gmail.com>
2018-08-23 22:01:13 -0700

    finished promises

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,23 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
-
-
-
+var pluckLine = require('./promiseConstructor.js');
+var gitHubAPI = require('./promisification.js');
+Promise.promisifyAll(fs);
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return pluckLine.pluckFirstLineFromFileAsync(readFilePath) 
+    .then(function (userName) {
+      console.log(userName);
+      return userName;
+    })
+    .then(function(user) {
+      return gitHubAPI.getGitHubProfileAsync(user);
+    })
+    .then(function(profile) {
+      console.log(profile);
+      // var writeFile = Promise.promisify(fs.writeFile);
+      return fs.writeFileAsync(writeFilePath, JSON.stringify(profile), 'utf8');
+    });
 };
 
 // Export these functions so we can test them
